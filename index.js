d4ac7fd451ca99efbb8a4c706a431b957b33879e
leo blanche <leo.firon@live.fr>
2026-02-09 17:06:20 +0100

    changement des notes, ajout text et style dashboard

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ imgNavLogo.addEventListener("click", () => {
             </div>
         </div>
         <div id="divSectionBoardActu" class="divSectionBoard">
-            <h2>Contact</h2>
+            <h2>Actualités</h2>
         </div>
     `;
 });
@@ -61,9 +61,15 @@ svgNavDashBoard.addEventListener("click", () => {
         <path d="M320 112C434.9 112 528 205.1 528 320C528 434.9 434.9 528 320 528C205.1 528 112 434.9 112 320C112 205.1 205.1 112 320 112zM320 576C461.4 576 576 461.4 576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 461.4 178.6 576 320 576zM231 231C221.6 240.4 221.6 255.6 231 264.9L286 319.9L231 374.9C221.6 384.3 221.6 399.5 231 408.8C240.4 418.1 255.6 418.2 264.9 408.8L319.9 353.8L374.9 408.8C384.3 418.2 399.5 418.2 408.8 408.8C418.1 399.4 418.2 384.2 408.8 374.9L353.8 319.9L408.8 264.9C418.2 255.5 418.2 240.3 408.8 231C399.4 221.7 384.2 221.6 374.9 231L319.9 286L264.9 231C255.5 221.6 240.3 221.6 231 231z" />`;
         document.querySelector("#sectionBoard").innerHTML = `
             <div id="divSectionBoardDashBoard" class="">
-                <div id="divSectionBoardDashBoardOne" class="divSectionBoardDashBoardElements"></div>
-                <div id="divSectionBoardDashBoardTwo" class="divSectionBoardDashBoardElements"></div>
-                <div id="divSectionBoardDashBoardThree" class="divSectionBoardDashBoardElements"></div>
+                <div id="divSectionBoardDashBoardOne" class="divSectionBoardDashBoardElements">
+                    <h2>Livraison de repas à domicile</h2>
+                </div>
+                <div id="divSectionBoardDashBoardTwo" class="divSectionBoardDashBoardElements">
+                    <h2>Entretien d'espaces verts</h2>
+                </div>
+                <div id="divSectionBoardDashBoardThree" class="divSectionBoardDashBoardElements">
+                    <h2>Contact</h2>
+                </div>
             </div>
         `;
         /*****/
@@ -133,7 +139,7 @@ svgNavDashBoard.addEventListener("click", () => {
                 </div>
             </div>
             <div id="divSectionBoardActu" class="divSectionBoard">
-                <h2>Contact</h2>
+                <h2>Actualités</h2>
             </div>
         `;
         /**********************************/
@@ -159,7 +165,7 @@ svgNavDashBoard.addEventListener("click", () => {
                 </div>
             </div>
             <div id="divSectionBoardActu" class="divSectionBoard">
-                <h2>Contact</h2>
+                <h2>Actualités</h2>
             </div>
         `;
         /**********************************/
@@ -177,10 +183,5 @@ svgNavDashBoard.addEventListener("click", () => {
 
 
 
-        -------------
-
-
-    Menu inutile ? 
-     - remplacer le svgDB par un svg home accueil. 
-     - ajouter un btn precedent ? 
+        
 */
