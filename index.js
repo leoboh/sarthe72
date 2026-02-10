320f129adb14a6f52fd8189042a75b6032fddbe8
leo blanche <leo.firon@live.fr>
2026-02-10 14:50:59 +0100

    Ajout condition sur sectionBoardContact + ajout remove classList sur imgNavLogo

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ const svgNavDashBoard = document.querySelector("#svgNavDashBoard");
 imgNavLogo.addEventListener("click", () => {
     svgNavDashBoard.classList.remove("svgNavDashBoardOpen");
     svgNavDashBoard.classList.add("svgNavDashBoardClose");
+    sectionBoard.classList.remove("sectionBoardLunch");
+    sectionBoard.classList.remove("sectionBoardGreenSpace");
+    sectionBoard.classList.remove("sectionBoardContact");
+    sectionBoard.classList.add("sectionBoardHome");
     document.querySelector("#svgNavDashBoard").innerHTML = `
         <path d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z"/>`;
     document.querySelector("#sectionBoard").innerHTML = `
@@ -307,6 +311,85 @@ svgNavDashBoard.addEventListener("click", () => {
         <path d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z"/>`;
         document.querySelector("#sectionBoard").innerHTML = `
             <div id="divSectionBoardGreenSpace" class="">
+        `;
+        /*************** CONTACT *******************/
+        /*************** CONTACT *******************/
+        /*************** CONTACT *******************/
+    } else if (document.querySelector(".svgNavDashBoardClose") && document.querySelector(".sectionBoardContact")) {
+        svgNavDashBoard.classList.remove("svgNavDashBoardClose");
+        svgNavDashBoard.classList.add("svgNavDashBoardOpen");
+        document.querySelector("#svgNavDashBoard").innerHTML = `
+            <path d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z"/>`;
+        document.querySelector("#sectionBoard").innerHTML = `
+            <div id="divSectionBoardDashBoard" class="">
+                <div id="divSectionBoardDashBoardOne" class="divSectionBoardDashBoardElements">
+                    <h2>Livraison de repas à domicile</h2>
+                </div>
+                <div id="divSectionBoardDashBoardTwo" class="divSectionBoardDashBoardElements">
+                    <h2>Entretien d'espaces verts</h2>
+                </div>
+                <div id="divSectionBoardDashBoardThree" class="divSectionBoardDashBoardElements">
+                    <h2>Contact</h2>
+                </div>
+            </div>
+        `;
+
+        /*****/
+        /*****/
+        const divSectionBoardDashBoardOne = document.querySelector("#divSectionBoardDashBoardOne");
+        const divSectionBoardDashBoardTwo = document.querySelector("#divSectionBoardDashBoardTwo");
+        const divSectionBoardDashBoardThree = document.querySelector("#divSectionBoardDashBoardThree");
+        /*****/
+        /*****/
+
+        /* LUNCH */
+        divSectionBoardDashBoardOne.addEventListener("click", () => {
+            svgNavDashBoard.classList.remove("svgNavDashBoardOpen");
+            svgNavDashBoard.classList.add("svgNavDashBoardClose");
+            sectionBoard.classList.remove("sectionBoardContact");
+            sectionBoard.classList.add("sectionBoardLunch");
+            document.querySelector("#svgNavDashBoard").innerHTML = `
+                <path d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z"/>`;
+            document.querySelector("#sectionBoard").innerHTML = `
+                <div id="divSectionBoardLunch" class="">
+            `;
+        });
+
+        /*****/
+        /* GREEN.SPACE */
+        divSectionBoardDashBoardTwo.addEventListener("click", () => {
+            svgNavDashBoard.classList.remove("svgNavDashBoardOpen");
+            svgNavDashBoard.classList.add("svgNavDashBoardClose");
+            sectionBoard.classList.remove("sectionBoardContact");
+            sectionBoard.classList.add("sectionBoardGreenSpace");
+            document.querySelector("#svgNavDashBoard").innerHTML = `
+                <path d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z"/>`;
+            document.querySelector("#sectionBoard").innerHTML = `
+                <div id="divSectionBoardGreenSpace" class="">
+            `;
+        });
+
+        /*****/
+        /* CONTACT */
+        divSectionBoardDashBoardThree.addEventListener("click", () => {
+            svgNavDashBoard.classList.remove("svgNavDashBoardOpen");
+            svgNavDashBoard.classList.add("svgNavDashBoardClose");
+            document.querySelector("#svgNavDashBoard").innerHTML = `
+                <path d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z"/>`;
+            document.querySelector("#sectionBoard").innerHTML = `
+                <div id="divSectionBoardContact" class="">
+            `;
+        });
+        /*************** CONTACT2 *******************/
+        /*************** CONTACT2 *******************/
+        /*************** CONTACT2 *******************/
+    } else if (document.querySelector(".svgNavDashBoardOpen") && document.querySelector(".sectionBoardContact")) {
+        svgNavDashBoard.classList.remove("svgNavDashBoardOpen");
+        svgNavDashBoard.classList.add("svgNavDashBoardClose");
+        document.querySelector("#svgNavDashBoard").innerHTML = `
+        <path d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z"/>`;
+        document.querySelector("#sectionBoard").innerHTML = `
+            <div id="divSectionBoardContact" class="">
         `;
     } else {
         svgNavDashBoard.classList.add("svgNavDashBoardClose");
